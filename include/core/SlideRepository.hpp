5f5225dba9e4889b2e292fa4c26b1315530bd281
Narek <165073089+buniatyann@users.noreply.github.com>
2025-09-09 21:52:44 +0400

    changes in SlideRepository

--- include/core/SlideRepository.hpp
+++ include/core/SlideRepository.hpp
@@ -4,13 +4,14 @@
 #include "../interfaces/ISlideRepository.hpp"
 
 class SlideRepository : public ISlideRepository {
-private:
-    std::vector<std::unique_ptr<Slide>> slides_;
 public:
     void addSlide(std::unique_ptr<Slide> slide) override;
     Slide* getSlideById(int id) override;
     const std::vector<std::unique_ptr<Slide>>& getAllSlides() const override;
     void displayAll() const override;
+    
+private:
+    std::vector<std::unique_ptr<Slide>> slides_;
 };
 
 #endif // SLIDEEDITOR_CORE_SLIDE_REPOSITORY_HPP
