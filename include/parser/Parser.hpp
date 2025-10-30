3a6401d37dc19d8e3a45c0d513ccde3ec740a99d
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-10-30 19:23:31 +0400

    +CMake Demo

--- include/parser/Parser.hpp
+++ include/parser/Parser.hpp
@@ -1,43 +1,30 @@
-#ifndef SLIDEEDITOR_PARSER_PARSER_HPP
-#define SLIDEEDITOR_PARSER_PARSER_HPP
+#ifndef PARSER_HPP
+#define PARSER_HPP
 
-#include "Lexer.hpp"
-#include "../interfaces/ICommand.hpp"
-#include "../core/SlideFactory.hpp"
+#include "Tokenizer.hpp"
+#include "../error/Error.hpp"
+#include "../model/SlideFactory.hpp"
 #include "../interfaces/ISlideRepository.hpp"
 #include "../serialization/JsonSerializer.hpp"
-#include <istream>
+#include <expected>
+#include <memory>
+
+class ICommand;
 
 class Parser {
 public:
-    Parser(SlideFactory& slideFactory, ISlideRepository& repo, JsonSerializer& serializer);
-    std::unique_ptr<ICommand> parse(std::istream& input) const;
-    void displayHelp(const std::string& command = "") const;
+    Parser(SlideFactory& f, ISlideRepository& r, JsonSerializer& s);
+    std::expected<std::unique_ptr<ICommand>, Error> parse(std::istream& input);
 
 private:
-    enum class State {
-        START,
-        CREATE_TITLE, CREATE_CONTENT, CREATE_THEME,
-        ADDTEXT_ID, ADDTEXT_TEXT, ADDTEXT_SIZE_FLAG, ADDTEXT_SIZE_VALUE,
-        ADDTEXT_FONT_FLAG, ADDTEXT_FONT_VALUE,
-        ADDTEXT_COLOR_FLAG, ADDTEXT_COLOR_VALUE,
-        ADDTEXT_LINEWIDTH_FLAG, ADDTEXT_LINEWIDTH_VALUE,
-        ADDSHAPE_ID, ADDSHAPE_TYPE, ADDSHAPE_SCALE,
-        MOVE_FROM, MOVE_TO,
-        REMOVETEXT_ID, REMOVETEXT_INDEX,
-        REMOVESHAPE_ID, REMOVESHAPE_INDEX,
-        SAVE_FILE,
-        LOAD_FILE,
-        DISPLAY,
-        HELP, HELP_COMMAND,
-        ERROR
-    };
-
-    // void displayHelp(const std::string& command = "") const; initially it was here not in public field
-    SlideFactory& slideFactory_;
+    SlideFactory& factory_;
     ISlideRepository& repo_;
     JsonSerializer& serializer_;
+
+    std::expected<std::unique_ptr<ICommand>, Error> parseCreate(Tokenizer& t);
+    std::expected<std::unique_ptr<ICommand>, Error> parseMove(Tokenizer& t);
+    std::expected<std::unique_ptr<ICommand>, Error> parseDisplay();
+    std::expected<std::unique_ptr<ICommand>, Error> parseSave(Tokenizer& t);
 };
 
-
-#endif // SLIDEEDITOR_PARSER_PARSER_HPP
+#endif
