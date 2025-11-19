35e359313eb6f3ba39dfbcfd1bd9beedce29cf21
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-11-19 18:45:12 +0400

    changes in IShape

--- modules/core/include/interfaces/IShape.hpp
+++ modules/core/include/interfaces/IShape.hpp
@@ -13,19 +13,58 @@ enum class ShapeType {
     ELLIPSE
 };
 
+inline std::string shapeTypeToString(ShapeType type) {
+    switch (type) {
+        case ShapeType::CIRCLE: 
+            return "circle";
+        case ShapeType::RECTANGLE: 
+            return "rectangle";
+        case ShapeType::TRIANGLE: 
+            return "triangle";
+        case ShapeType::ELLIPSE: 
+            return "ellipse";
+        default: 
+            return "unknown";
+    }
+}
+
+inline ShapeType stringToShapeType(const std::string& str) {
+    std::string lower = str;
+    std::transform(lower.begin(), lower.end(), lower.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+    
+    if (lower == "circle") {
+        return ShapeType::CIRCLE;
+    }
+    if (lower == "rectangle") {
+        return ShapeType::RECTANGLE;
+    }
+    if (lower == "triangle") {
+        return ShapeType::TRIANGLE;
+    }
+    if (lower == "ellipse") {
+        return ShapeType::ELLIPSE;
+    }
+    
+    return ShapeType::CIRCLE;  // Default
+}
+
 class IShape {
 public:
     virtual ~IShape() = default;
     
     virtual ShapeType getType() const = 0;
     virtual double getScale() const = 0;
-    virtual std::string toString() const = 0;
-    
     virtual void setScale(double scale) = 0;
-    
+    virtual std::string getBorderColor() const = 0;
+    virtual std::string getFillColor() const = 0;
+    virtual void setBorderColor(const std::string& color) = 0;
+    virtual void setFillColor(const std::string& color) = 0;
+    virtual std::string toString() const = 0;
     virtual std::string serialize() const = 0;
-    
     virtual std::unique_ptr<IShape> clone() const = 0;
+    // SVG generation
+    virtual std::string toSVG(double x, double y) const = 0;
 };
 
 } // namespace slideEditor::core
