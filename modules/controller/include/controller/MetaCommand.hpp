79070ed1ffcde2731780cb44f2c396b23d3a1460
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-11-23 15:01:22 +0400

    -commandContext declaration from MetaCommand

--- modules/controller/include/controller/MetaCommand.hpp
+++ modules/controller/include/controller/MetaCommand.hpp
@@ -2,56 +2,25 @@
 #define META_COMMAND_HPP
 
 #include "interfaces/IMetaCommand.hpp"
+#include "controller/CommandContext.hpp"
 #include <sstream>
 #include <algorithm>
+#include <initializer_list>
 
 namespace slideEditor::controller {
-
-class CommandContext {
-public:
-    CommandContext();
-    ~CommandContext() = default;
-    
-    void setRepository(std::shared_ptr<core::ISlideRepository> repository);
-    void setSerializer(std::shared_ptr<core::ISerializer> serializer);
-    void setView(std::shared_ptr<core::IView> view);
-    void setHistory(std::shared_ptr<CommandHistory> history);
-    void setRegistry(CommandRegistry* registry);
-    
-    std::shared_ptr<core::ISlideRepository> getRepository() const;
-    std::shared_ptr<core::ISerializer> getSerializer() const;
-    std::shared_ptr<core::IView> getView() const;
-    std::shared_ptr<CommandHistory> getHistory() const;
-    CommandRegistry* getRegistry() const;
-    
-    bool isValid() const;
-    std::string getMissingDependencies() const;
-    
-    bool hasRepository() const;
-    bool hasSerializer() const;
-    bool hasView() const;
-    bool hasHistory() const;
-    bool hasRegistry() const;
-
-private:
-    std::shared_ptr<core::ISlideRepository> repository_;
-    std::shared_ptr<core::ISerializer> serializer_;
-    std::shared_ptr<core::IView> view_;
-    std::shared_ptr<CommandHistory> history_;
-    CommandRegistry* registry_;
-};
-
 
 class MetaCommand : public core::IMetaCommand {
 public:
     MetaCommand(std::string name, 
                 std::string description,
                 std::string category,
-                core::CommandCreator creator)
+                core::CommandCreator creator,
+                std::initializer_list<core::ArgumentInfo> arguments = {})
         : name_(std::move(name)),
           description_(std::move(description)),
           category_(std::move(category)),
-          creator_(std::move(creator)) {}
+          creator_(std::move(creator)),
+          arguments_(arguments) {}
     
     virtual ~MetaCommand() = default;
     
@@ -83,7 +52,6 @@ public:
             return false;
         }
         
-        // Type validation (basic)
         for (size_t i = 0; i < args.size(); ++i) {
             const auto& argInfo = arguments_[i];
             if (!validateArgumentType(args[i], argInfo.type)) {
@@ -129,10 +97,6 @@ public:
         return oss.str();
     }
 
-    void addArgument(const core::ArgumentInfo& arg) {
-        arguments_.push_back(arg);
-    }
-
 protected:
     std::string name_;
     std::string description_;
@@ -141,7 +105,7 @@ protected:
     core::CommandCreator creator_;
 
 protected:
-    
+
     // Type validation helper
     bool validateArgumentType(const std::string& value, const std::string& type) const {
         if (type == "string" || type == "identifier") {
@@ -164,9 +128,8 @@ protected:
             }
         }
         
-        return true;  // Unknown type, allow
+        return true;
     }
-
 };
 
 } // namespace slideEditor::controller
